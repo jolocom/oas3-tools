a311db89baefc87decc7714d62084f360d0e8e13
Serhii Temchenko <serg.temchenko@gmail.com>
2021-12-16 12:03:58 +0100

    gitpkg

---
+++ dist/middleware/logging.options.js
@@ -0,0 +1,13 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.LoggingOptions = void 0;
+class LoggingOptions {
+    format;
+    errorLimit;
+    constructor(format, errorLimit) {
+        this.format = format;
+        this.errorLimit = errorLimit;
+    }
+}
+exports.LoggingOptions = LoggingOptions;
+//# sourceMappingURL=logging.options.js.map
